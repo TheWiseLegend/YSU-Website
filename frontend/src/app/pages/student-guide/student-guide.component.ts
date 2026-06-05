ad0f6f0e079c5313a7ea5724e11f9fcff106dcf2
devAmjad4590 <developeramjad4590@gmail.com>
2026-06-05 06:34:44 +0300

    redesigned the student guide page

--- frontend/src/app/pages/student-guide/student-guide.component.ts
+++ frontend/src/app/pages/student-guide/student-guide.component.ts
@@ -1,11 +1,13 @@
 // src/app/pages/student-guide/student-guide.component.ts
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { PageHeaderComponent } from '../../components/page-header/page-header.component';
+
 @Component({
   selector: 'app-student-guide',
   standalone: true,
-  imports: [CommonModule, PageHeaderComponent],
+  imports: [CommonModule, RouterLink, PageHeaderComponent],
   templateUrl: './student-guide.component.html',
   styleUrls: ['./student-guide.component.scss']
 })
